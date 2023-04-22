8d0c6f4f28968f26230b76bc8587ad51bb381110
Samuel Howard <sahoward42@tntech.edu>
2023-04-22 08:32:07 -0500

    Added audio method

---
+++ src/methods/audio/audio.py
@@ -0,0 +1,42 @@
+from src.methods.method import Method
+from src.passage import PassageInvalid, PassageNotFound
+import requests
+
+
+class Audio(Method):
+    def __init__(self, api_key: str) -> None:
+        """
+        :param api_key: ESV API key
+        """
+        super().__init__()
+        self.__API_KEY: str = api_key
+        self.__API_URL: str = 'https://api.esv.org/v3/passage/audio/'
+
+    def get_passage(self, book: str, chapter: int, verse: int = None) -> str:
+        """
+        Gets the audio version of a passage from the ESV API. This method only takes a subset of possible queries since
+        the API does no validation and basically makes a URL out of your query, even if it is invalid. Hence, this
+        function will raise its own exception if you make a bad query. This is so you don't get bad links.
+        :param book: Name of the book to get
+        :param chapter: The chapter to get
+        :param verse: verse to get (optional)
+        :return: the URL of that passage
+        :raises: PassageInvalid for invalid passage queries.
+        :raises: PassageNotFound for connection issues.
+        """
+        headers: dict = {'Authorization': 'Token %s' % self.__API_KEY}
+        verse = verse if verse else ""
+        query: str = "{} {}".format(book, str(chapter) + (":" if verse else "") + str(verse))
+        params = {'q': query}
+
+        if not super().has_passage(book, chapter):
+            raise PassageInvalid(f"{book} {chapter}")
+
+        try:
+            response: str = requests.get(self.__API_URL, params=params, headers=headers).url
+            if response:
+                return response
+            else:
+                raise PassageNotFound(query)
+        except requests.HTTPError:
+            raise PassageNotFound(query)
---
+++ tests/test_audio.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from src.methods.audio.audio import Audio
+from src.passage import PassageInvalid
+
+
+class TestAudio(TestCase):
+    def setUp(self) -> None:
+        with open("api-key.txt", "r") as key_in:
+            key = key_in.read()
+        self.audio_obj = Audio(key)
+
+    def test_get_passage(self):
+        passage = self.audio_obj.get_passage("John", 11, 35)
+        self.assertEqual("https://audio.esv.org/hw/mq/John%2011:35.mp3", passage)
+        passage2 = self.audio_obj.get_passage("John", 11)
+        self.assertEqual("https://audio.esv.org/hw/mq/John%2011.mp3", passage2)
+
+        with self.assertRaises(PassageInvalid):
+            self.audio_obj.get_passage("Book", 25)
